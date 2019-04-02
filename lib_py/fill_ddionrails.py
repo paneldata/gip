fb0f7a7554c5b79cff0a66fec3456b2a01dcc3f2
mhebing <marcel.hebing@gmail.com>
2019-04-02 14:22:21 +0200

    Update metadata

--- lib_py/fill_ddionrails.py
+++ lib_py/fill_ddionrails.py
@@ -7,6 +7,8 @@ from ddi.onrails.repos import merge_instruments, dor1, copy, extract_variables, 
 
 def main():
     copy.study()
+    copy.f("datasets.csv")
+    copy.f("instruments.csv")
     copy.f("questions_variables.csv")
     convert_r2ddi.Parser("gip", version="v1").write_json()
     extract_variables.XmlParser("r2ddi/v1/en", "gip").run()
