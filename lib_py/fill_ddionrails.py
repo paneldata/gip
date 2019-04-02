ba03e8ba9cc7aa6884446367d3bb08af5d09c5f9
mhebing <marcel.hebing@gmail.com>
2019-04-02 14:33:54 +0200

    Update datasets.csv

--- lib_py/fill_ddionrails.py
+++ lib_py/fill_ddionrails.py
@@ -7,12 +7,12 @@ from ddi.onrails.repos import merge_instruments, dor1, copy, extract_variables, 
 
 def main():
     copy.study()
-    copy.f("datasets.csv")
     copy.f("instruments.csv")
     copy.f("questions_variables.csv")
     convert_r2ddi.Parser("gip", version="v1").write_json()
     extract_variables.XmlParser("r2ddi/v1/en", "gip").run()
     merge_instruments.main()
+    copy.f("datasets.csv")
 
 if __name__ == "__main__":
     main()
