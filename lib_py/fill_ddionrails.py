0747c9abb15590b8dbbbad6ee4fb408c77a00583
mhebing <marcel.hebing@gmail.com>
2020-02-04 14:52:42 +0100

    Update fill_ddionrails.py to convert datasets to UTF-8

--- lib_py/fill_ddionrails.py
+++ lib_py/fill_ddionrails.py
@@ -1,9 +1,18 @@
 import os, sys
+import codecs
 import pandas as pd
 
 sys.path.append(os.path.expanduser("../ddi.py/"))
 
 from ddi.onrails.repos import merge_instruments, dor1, copy, extract_variables, convert_r2ddi
+
+def recode_datasets(in_enc="windows-1252", out_enc="utf-8"):
+    dpath = os.path.join("ddionrails", "datasets")
+    for filename in os.listdir(dpath):
+        filepath = os.path.join(dpath, filename)
+        content = codecs.open(filepath, encoding=in_enc, errors="strict").read()
+        with codecs.open(filepath, "w+", encoding=out_enc) as f:
+            f.write(content)
 
 def main():
     copy.study()
@@ -11,6 +20,7 @@ def main():
     copy.f("questions_variables.csv")
     copy.f("attachments.csv")
     convert_r2ddi.Parser("gip").write_json()
+    recode_datasets()
     extract_variables.XmlParser("r2ddi/v1/en", "gip").run()
     copy.f("datasets.csv")
     merge_instruments.main()
